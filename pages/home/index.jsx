1919c32aa60b3dc899acf4197f793a7ff2f09833
Pranav <prnvshanmukh00@gmail.com>
2024-05-06 18:58:52 +0530

    chore: all done

---
+++ pages/home/index.jsx
@@ -0,0 +1,45 @@
+import React ,{useState} from "react";
+import Router from "next/router";
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
+import "ag-grid-community/styles/ag-theme-quartz.css";
+
+export default function Home() {
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+  return (
+    <div className="w-full h-screen flex items-center justify-center text-white bg-indigo-700 flex-col tracking-widest uppercase">
+      <div
+        className="ag-theme-quartz w-full h-1/2 lg:w-1/2"
+      >
+        <AgGridReact rowData={rowData} columnDefs={colDefs} />
+      </div>
+      <button
+        onClick={() => Router.push("/")}
+        className="absolute top-2 right-2 bg-white border-2 border-white hover:bg-transparent transition-all text-indigo-700 hover:text-white font-semibold text-lg  px-4 py-2 rounded duration-700"
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
